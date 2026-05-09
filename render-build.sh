2ae237460580656bcbf0ee8ffeeb9b4e6e0357c2
charactera267-commits <charactera267@gmail.com>
2026-05-09 16:57:08 +0530

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,2 +1,10 @@
 #!/usr/bin/env bash
+
+# 1. Zip file ko extract (unzip) karo using Python
+python -m zipfile -e Aditya-AI-Studio.zip .
+
+# 2. Extracted folder ke andar se files ko bahar nikalo
+mv Aditya-AI-Studio/* .
+
+# 3. Python tools install karo
 pip install -r requirements.txt
